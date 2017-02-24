96f19a8bc857657956fd0a070e80842cafccee3b
binq1000 <t.daniels@student.fontys.nl>
2017-02-24 10:38:32 +0100

    Added system outs to see if they're running

--- src/test/java/NumberGeneratorTests.java
+++ src/test/java/NumberGeneratorTests.java
@@ -8,17 +8,20 @@ import org.junit.Test;
 public class NumberGeneratorTests {
 	@Test
 	public void TestNumberOne() {
+		System.out.println("Error throwing test");
 		NumberGenerator ng = new NumberGenerator();
 		Assert.assertTrue(ng.GetOne() == 1);
 	}
 
 	@Test
 	public void ErrorThrowing() {
+		System.out.println("Error throwing test");
 		Assert.assertTrue(false);
 	}
 
 	@Test
 	public void anotherError() {
+		System.out.println("Error throwing test");
 		Assert.assertFalse(true);
 	}
 
