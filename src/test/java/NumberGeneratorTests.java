576ff18cff6cc213e67b8529e99fc22951e507df
binq1000 <t.daniels@student.fontys.nl>
2017-02-24 10:20:33 +0100

    Added a test

---
+++ src/test/java/NumberGeneratorTests.java
@@ -0,0 +1,15 @@
+import TestPackage.NumberGenerator;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Nekkyou on 24-2-2017.
+ */
+public class NumberGeneratorTests {
+	@Test
+	public void TestNumberOne() {
+		NumberGenerator ng = new NumberGenerator();
+		Assert.assertTrue(ng.GetOne() == 1);
+	}
+
+}
